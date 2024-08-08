cd2e7bb94554f168a249218bb8b5b615bc70e229
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2024-08-08 18:58:23 +0300

    Setup favicon

--- client/src/layouts/Root/RootLayout.tsx
+++ client/src/layouts/Root/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactQueryProvider } from "@/providers";
 import { ReactNode } from "react";
+import favicon from "../../../public/favicon.ico"
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -7,6 +8,9 @@ type RootLayoutProps = {
 export const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src} sizes="any" />
+      </head>
       <body>
         <ReactQueryProvider>
           <main>{children}</main>
