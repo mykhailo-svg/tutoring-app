09f3385b6e3de935941f9c5d1ba047b622ce7961
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2024-08-09 20:02:23 +0300

    Imporving register page

--- client/src/routes/register/RegisterPage.tsx
+++ client/src/routes/register/RegisterPage.tsx
@@ -1,5 +1,8 @@
-"use client";
-import { useUserRegister } from "./hooks/useUserRegister";
+'use client';
+import { AuthRow } from '@/components/AuthRow';
+import * as Form from '@radix-ui/react-form';
+import { useUserRegister } from './hooks/useUserRegister';
+import styles from "./RegisterPage.module.scss"
 
 type RegisterPageProps = {};
 
@@ -7,8 +10,20 @@ export const RegisterPage: React.FC<RegisterPageProps> = () => {
   const { registerUser } = useUserRegister();
 
   return (
-    <div>
-      <button onClick={registerUser}>register</button>
-    </div>
+    <AuthRow
+      firstColumnContent={
+        <div className={styles.formColumn}>
+          <Form.Root className={styles.form}>
+            <Form.Field name='name'>
+              <Form.Label className='FormLabel'>Email</Form.Label>
+              <Form.Control asChild>
+                <input className='Input' type='email' required />
+              </Form.Control>
+            </Form.Field>
+          </Form.Root>
+        </div>
+      }
+      secondColumnContent={<div className={styles.questionColumn}> sign</div>}
+    />
   );
 };
