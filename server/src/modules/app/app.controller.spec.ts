ae20d8a562ee17b31e89015614e8376e32db9a68
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2024-08-16 23:17:43 +0300

    Testing

--- server/src/modules/app/app.controller.spec.ts
+++ server/src/modules/app/app.controller.spec.ts
@@ -1,22 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Request } from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleMixture: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env',
+        }),
+        TypeOrmModule.forRoot({
+          //@ts-ignore
+          type: process.env.POSTGRES_TESTING_TYPE,
+          host: process.env.POSTGRES_TESTING_HOST,
+          port: Number(process.env.POSTGRES_TESTING_PORT),
+          username: process.env.POSTGRES_TESTING_USER,
+          password: process.env.POSTGRES_TESTING_PASSWORD,
+          database: process.env.POSTGRES_TESTING_DB,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    app = moduleMixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.init();
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should return "Hello World!"', async () => {
+    const response = await request(app.getHttpServer()).get('/').expect(200);
+
+    console.log(response.body);
+    console.log(response.status);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
