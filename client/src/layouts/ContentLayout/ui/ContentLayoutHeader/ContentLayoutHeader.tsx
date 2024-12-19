c5b5fb57732424a10f36d621642f4de7dc2032cb
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2024-12-19 20:11:35 +0200

    client:create popover compoentn

--- client/src/layouts/ContentLayout/ui/ContentLayoutHeader/ContentLayoutHeader.tsx
+++ client/src/layouts/ContentLayout/ui/ContentLayoutHeader/ContentLayoutHeader.tsx
@@ -1,7 +1,12 @@
+import { Popover } from '@/shared/ui/popovers';
 import styles from './ContentLayoutHeader.module.scss';
 
 type ContentLayoutHeaderProps = {};
 
 export const ContentLayoutHeader: React.FC<ContentLayoutHeaderProps> = () => {
-  return <header className={styles.root}>header</header>;
+  return (
+    <header className={styles.root}>
+      <Popover activator={<div>click</div>} >hello</Popover>
+    </header>
+  );
 };
