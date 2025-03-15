15611e8e65c6357784e7ed8d4dbff0c2d8e89c43
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2025-03-15 20:07:00 +0200

    Basic sticky date badge

--- client/src/routes/chat/ui/ChatMessagesList/ChatMessagesList.tsx
+++ client/src/routes/chat/ui/ChatMessagesList/ChatMessagesList.tsx
@@ -4,7 +4,7 @@ import type { DirectMessage, User } from '@/global_types';
 import { useAuth } from '@/providers/AuthProvider';
 import classNames from 'classnames';
 import { Scrollable, ScrollableOnScrolledToTop } from '@/shared/ui/scrollable/Scrollable';
-import { Fragment, useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import { Fragment, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { APIEndpoints, axiosClient } from '@/api';
 import { translateMonth } from '@/shared/helpers/translateMonth';
 
@@ -19,6 +19,8 @@ export const ChatMessagesList: React.FC<ChatMessagesListProps> = ({
   companion,
   fetchNextMessages,
 }) => {
+  const [stickyDate, setStickyDate] = useState('');
+
   const scrollableRef = useRef<HTMLDivElement>(null);
 
   // useEffect(() => {
@@ -38,14 +40,22 @@ export const ChatMessagesList: React.FC<ChatMessagesListProps> = ({
 
   return (
     <Card shadow='none' className={styles.root}>
+      {stickyDate && (
+        <div className={styles.stickyDateBadge}>
+          <span>{getUserFriendlyDate(stickyDate)}</span>
+        </div>
+      )}
       <Scrollable onScrolledToTop={onScrolledToTop} ref={scrollableRef} className={styles.inner}>
         <div className={styles.list}>
           {messages.map((message, index) => (
             <Fragment key={message.id}>
               {shouldDisplayDateBadge(messages[index - 1], message, index) && (
-                <div className={styles.dateBadge}>
-                  <span>{getUserFriendlyDate(message.createdAt)}</span>
-                </div>
+                <>
+                  <div className={styles.dateBadge}>
+                    <span>{getUserFriendlyDate(message.createdAt)}</span>
+                  </div>
+                  <DateAnchor setDate={setStickyDate} date={message.createdAt} />
+                </>
               )}
 
               <div
@@ -58,6 +68,12 @@ export const ChatMessagesList: React.FC<ChatMessagesListProps> = ({
                   <span className={styles.time}>{convertToLocalTime(message.createdAt)}</span>
                 </div>
               </div>
+
+              {index < messages.length - 1 &&
+                formatDateForCompare(messages[index + 1].createdAt) !==
+                  formatDateForCompare(message.createdAt) && (
+                  <DateAnchor setDate={setStickyDate} date={message.createdAt} />
+                )}
             </Fragment>
           ))}
         </div>
@@ -69,7 +85,7 @@ export const ChatMessagesList: React.FC<ChatMessagesListProps> = ({
 function formatDateForCompare(string: string) {
   const date = new Date(string);
 
-  return `${date.getDate()}`;
+  return `${date.getMinutes()}`;
 }
 
 function shouldDisplayDateBadge(
@@ -86,7 +102,7 @@ function shouldDisplayDateBadge(
 function getUserFriendlyDate(stringifiedDate: string) {
   const date = new Date(stringifiedDate);
 
-  return `${date.getDate()} ${translateMonth(date.getMonth())}`;
+  return `${date.getMinutes()} ${translateMonth(date.getMonth())}`;
 }
 
 function convertToLocalTime(utcTimestamp: string): string {
@@ -98,4 +114,29 @@ function convertToLocalTime(utcTimestamp: string): string {
   return `${hours.length > 1 ? hours : '0' + hours}:${
     minutes.length > 1 ? minutes : '0' + minutes
   }`;
+}
+
+function DateAnchor({ date, setDate }: { date: string; setDate: (date: string) => void }) {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setDate(date);
+      },
+      { threshold: 0.1 } // Adjust threshold as needed
+    );
+
+    if (divRef.current) {
+      observer.observe(divRef.current);
+    }
+
+    return () => {
+      if (divRef.current) {
+        observer.unobserve(divRef.current);
+      }
+    };
+  }, [date, setDate]);
+
+  return <div ref={divRef}></div>;
 }
